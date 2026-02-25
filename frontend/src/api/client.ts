cc0aa9772aa47670f9b0b9988adb08c132ca029e
Aline Ribeiro <alinechribeiro@gmail.com>
2026-02-25 21:49:50 +0000

    update: prd aws

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,4 @@
-const API = '/api'
+const API = (import.meta.env.VITE_API_BASE_URL ?? '').replace(/\/$/, '') + '/api'
 
 async function request<T>(path: string, options?: RequestInit): Promise<T> {
   const res = await fetch(API + path, {
